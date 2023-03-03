161096c5bb2e2348761eafe9c9c0426a05c55667
Ashik Sazal <sojolislm@gmail.com>
2023-03-03 12:20:34 +0600

    first commit

---
+++ 5. Database connection/storage/framework/views/d8caade610f68afa0c352caa80393682.php
@@ -0,0 +1,7 @@
+<h1>Age form page</h1>
+<form action="" method="POST">
+    <input type="number" name="age">
+    <br><br>
+    <button type="submit">Submit</button>
+</form>
+<?php /**PATH D:\Projects\php\5. Global middleware\resources\views/age.blade.php ENDPATH**/ ?>
